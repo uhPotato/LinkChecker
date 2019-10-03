85bba8a21dd11f2e42fe24954786f53f7c800634
David Davies <d_davies@apple.com>
2019-10-02 17:36:22 -0700

    First commit for link checker

---
+++ linkChecker.js
@@ -0,0 +1,54 @@
+javascript:(function(){
+	var linkChecker = {
+		isChapterNavLink: function(l){
+			return l.getAttribute('class') != null || l.parentNode.className.match('chapternav') != null;
+		},
+		isAnalyticsRegion: function(l){
+			return l.parentNode.className.match('data-analytics-region') != null;
+		},
+		isAnalyticsTitle: function(l){
+			return l.getAttribute('data-analytics-title') != null;
+		},
+		isOtherSOmething: function(l){
+			
+		}
+	};
+	
+	var processedLinks = {};
+
+	var links = document.links;
+	var docTitle = document.title;
+	var header = document.createElement('h2');
+	var tblContainer = document.createElement('div');
+		
+	var tbl = '<table style="font-size: 16px; line-height:20px;">';
+	tbl += '<thead style="font-weight:bold;"><tr><td>Referenced Articles</td></tr></thead><tbody>';
+
+    console.log(links.length);
+    
+	for(var i = 0; i < links.length; i++){
+		var link = links[i];
+		if(typeof processedLinks[link.href] === 'undefined'){
+			processedLinks[link.href] = true;
+		
+			if(linkChecker.isChapterNavLink(link) &&
+				!linkChecker.isNavLink(link) &&
+				!linkChecker.isSpecialLink(link)){
+				
+				var href = link.getAttribute('href');
+				var title = link.getAttribute('title');
+                tbl += '<tr><td><a href="' + href + '">' + title + '</a></td></tr>';
+                console.log(link);
+			}
+		}
+	}
+	
+	tbl += '</tbody></table>';
+		
+	header.innerHTML = docTitle;
+	tblContainer.innerHTML = tbl;
+	document.body.innerHTML = '';
+	document.body.appendChild(header);
+	document.body.appendChild(tblContainer);
+	document.title = docTitle;
+}());
