cbc033f21ce6e4f9a1b49d24c5940a862f6f6d9b
David Davies <d_davies@apple.com>
2019-10-05 22:22:18 -0700

    attempting to add border to table

--- linkChecker.js
+++ linkChecker.js
@@ -11,7 +11,9 @@ javascript:(function(){
 	var header = document.createElement('h2');
 	var par = document.createElement('p');
 	var tblContainer = document.createElement('div');
-		
+	
+	var border = '<style>table, th, td { padding: 10px; border: 1px solid black; border-collapse: collapse;</style>';
+
 	var tbl = '<table style="font-size: 16px; line-height:20px;">';
 	tbl += '<thead style="font-weight:bold;"><tr><td>Link List</td></tr></thead><tbody>';
 
@@ -25,7 +27,7 @@ javascript:(function(){
 				var analytics = link.getAttribute('data-analytics-region');
 				var analyticsTitle = link.getAttribute('data-analytics-title');
 				var ariaLable = link.getAttribute('aria-label');
-                tbl += '<tr><td><a href="' + href + '">' + href + '</a></td><td>Analytics Title: ' + analyticsTitle + '</td><td>Analytics Region: ' + analytics + '</td><td> and Aria-Lable: ' + ariaLable + '</td></tr>';
+                tbl += '<tr><td><a href="' + href + '">' + href + '</a></td><td>Analytics Title: ' + analyticsTitle + '</td><td>Analytics Region: ' + analytics + '</td><td>Aria-Lable: ' + ariaLable + '</td></tr>';
                 console.log(link.href);
 			}
 	}
