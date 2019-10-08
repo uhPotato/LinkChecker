829d18788dffe58905fc5aad68b2606125ebde6e
David Davies <d_davies@apple.com>
2019-10-07 18:42:27 -0700

    Remove Footer links from report

--- linkChecker.js
+++ linkChecker.js
@@ -68,6 +68,8 @@ javascript:(function(){
 	header.innerHTML = docTitle;
 	tblBorder.innerHTML = border;
 	tblContainer.innerHTML = tbl;
+
+	resultsWindow.document.body.innerHTML = '';
 	resultsWindow.document.head.appendChild(tblBorder);
 	resultsWindow.document.body.appendChild(header);
 	resultsWindow.document.body.appendChild(tblContainer);
